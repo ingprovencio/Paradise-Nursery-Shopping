f2a74fd1f03a720eed7e23320317c68ba44e08bd
ingprovencio <85075557+ingprovencio@users.noreply.github.com>
2025-03-14 21:25:37 +0100

    Add files via upload

---
+++ src/CartSlice.jsx
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+    numOfItems: 0
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      }
+      else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+      state.numOfItems++
+    },
+    removeItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const itemToDelete = state.items.find(item => item.name === name);
+      state.items = state.items.filter(item => item.name !== name);
+      state.numOfItems += - itemToDelete.quantity;
+
+      console.log("borrado");
+    },
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        state.numOfItems += quantity - itemToUpdate.quantity;
+        itemToUpdate.quantity = quantity;
+      }
+
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
